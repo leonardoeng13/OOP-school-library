6ca7526739101cde23c49775dcb92e116caeff0c
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-11-17 15:34:56 -0300

    Basic UI

--- person.rb
+++ person.rb
@@ -1,8 +1,13 @@
+# rubocop:disable Style/OptionalBooleanParameter
+
 require './corrector'
 require './rental'
-# Person class to identify
+
 class Person
-  def initialize(age:, name: 'Unknown', parent_permission: true)
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
@@ -11,29 +16,23 @@ class Person
     @rentals = []
   end
 
-  attr_accessor :name, :age, :rentals
-  attr_reader :id
-
   def add_rental(date, book)
     Rental.new(date, self, book)
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
-
-  private
-
-  # I needed to do this is order to keep the method name required
-  # in the project
-  # rubocop:disable Naming/PredicateName
 
   def validate_name
     @name = @corrector.correct_name(@name)
   end
 
-  def is_of_age?
+  private
+
+  def of_age?
     @age >= 18
   end
-  # rubocop:enable Naming/PredicateName
 end
+
+# rubocop:enable Style/OptionalBooleanParameter

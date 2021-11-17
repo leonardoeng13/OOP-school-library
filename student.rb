ed411f2a114e12de060f4f15ac9173265a12a284
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-11-17 11:52:16 -0300

    attr_reader :classroom

--- student.rb
+++ student.rb
@@ -2,6 +2,9 @@ require_relative './person'
 
 # Student class to compare
 class Student < Person
+  
+  attr_reader :classroom
+
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
